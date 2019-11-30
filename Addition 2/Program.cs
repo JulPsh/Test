3c465ddfeb7f0f5e008fd16f2efc662c2bd84ec1
JulPsh <6523+JulPsh@users.noreply.github.com>
2019-11-30 17:53:02 +0300

    Test

---
+++ Addition 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Addition_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число 1:");
+            int x =int.Parse(Console.ReadLine());
+            Console.Write("Введите число 2:");
+            int y = int.Parse(Console.ReadLine());
+            int z = x + y;
+            Console.WriteLine($"{z}");
+            Console.ReadKey();
+        }
+    }
+}
